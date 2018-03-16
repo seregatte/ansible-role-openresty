9d82be138de77817311193a0a6cf3522d7903486
Joshua Haupt <joshua@hauptj.com>
2018-03-16 00:20:48 -0500

    Added download url to test

--- tests/HyperV/config.rb
+++ tests/HyperV/config.rb
@@ -1,6 +1,6 @@
 $centos_box = "hauptj/CentOS74"
 $centos_vmname = "OpenRestyCentOS"
-$ubuntu_box = "generic/ubuntu1604"
+$ubuntu_box = "bento/ubuntu-16.04"
 $ubuntu_vmname = "OpenRestyUbuntu"
 
 $vcpus = "2"
